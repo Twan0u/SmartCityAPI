a5926176f8d343572c52d3abd206bfd877f28d8a
Twan0u <ant.lamb.al@gmail.com>
2020-12-15 10:12:21 +0100

    cors enabled

--- server.js
+++ server.js
@@ -15,6 +15,22 @@ const swaggerDoc = require('./swagger/swagger_jsdoc').swaggerDoc;
 //Middlewares
 app.use(express.json());
 
+//CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+
 //Routes
 app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDoc));
 app.use(Router);
