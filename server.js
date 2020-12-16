fb637217e84f4fd99ed5c6c778cb73cddc6654cc
Twan0u <ant.lamb.al@gmail.com>
2020-12-16 23:29:59 +0100

    correction patch cors

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
     app.options('*', (req, res) => {
         // allowed XHR methods
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
         res.send();
     });
 });
